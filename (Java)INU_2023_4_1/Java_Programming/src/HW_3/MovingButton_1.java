6542ce93d487e862df5feb709fba816d1dd8df35
CarefreeLife <csm4903@naver.com>
2023-05-31 15:25:32 +0900

    Java HW_3

---
+++ (Java)INU_2023_4_1/Java_Programming/src/HW_3/MovingButton_1.java
@@ -0,0 +1,70 @@
+package HW_3;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class MovingButton_1 extends JFrame {
+    private final int MY_BUTTON = 10;
+    private final int MAX = 500;
+    private JButton jb = new JButton();
+
+    public MovingButton_1() {
+        setTitle("Moving Button");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container c = getContentPane();
+        c.setLayout(null);
+        c.setBackground(Color.DARK_GRAY);
+        c.addKeyListener(new movingKeyListener());
+
+        jb.setLocation(MAX / 2, MAX / 2);
+        jb.setSize(400, 50);
+        c.add(jb);
+
+        setSize(MAX, MAX);
+        setVisible(true);
+        c.setFocusable(true);
+        c.requestFocus(); // 강제 포커스 부여.
+    }
+
+    class movingKeyListener extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+            int keyCode = e.getKeyCode();
+
+            switch (keyCode) {
+                case KeyEvent.VK_UP:
+                    if (jb.getY() - MY_BUTTON >= 0) {
+                        jb.setLocation(jb.getX(), jb.getY() - MY_BUTTON);
+                        jb.setText(MAX + " X " + MAX + "범위 내 이동 가능, 현재: " + jb.getX() + "," + jb.getY());
+                        break;
+                    }break;
+                case KeyEvent.VK_DOWN:
+                    if (jb.getY() + MY_BUTTON <= MAX) {
+                        jb.setLocation(jb.getX(), jb.getY() + MY_BUTTON);
+                        jb.setText(MAX + " X " + MAX + "범위 내 이동 가능, 현재: " + jb.getX() + "," + jb.getY());
+                        break;
+                    }break;
+                case KeyEvent.VK_LEFT:
+                    if (jb.getX() - MY_BUTTON >= 0) {
+                        jb.setLocation(jb.getX() - MY_BUTTON, jb.getY());
+                        jb.setText(MAX + " X " + MAX + "범위 내 이동 가능, 현재: " + jb.getX() + "," + jb.getY());
+                        break;
+                    }break;
+                case KeyEvent.VK_RIGHT:
+                    if (jb.getX() + MY_BUTTON <= MAX) {
+                        jb.setLocation(jb.getX() + MY_BUTTON, jb.getY());
+                        jb.setText(MAX + " X " + MAX + "범위 내 이동 가능, 현재: " + jb.getX() + "," + jb.getY());
+                        break;
+                    }break;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        new MovingButton_1();
+    }
+}
---
+++ (Java)INU_2023_4_1/Java_Programming/src/HW_3/PrintWithButton.java
@@ -0,0 +1,5 @@
+package HW_3;
+
+public class PrintWithButton {
+
+}
