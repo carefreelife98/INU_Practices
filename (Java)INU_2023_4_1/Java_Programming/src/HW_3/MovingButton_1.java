1483787eea6dc2dbcbdfe0fecb952e3c65b9d189
CarefreeLife <csm4903@naver.com>
2023-06-02 02:16:06 +0900

    Java HW_3

--- (Java)INU_2023_4_1/Java_Programming/src/HW_3/MovingButton_1.java
+++ (Java)INU_2023_4_1/Java_Programming/src/HW_3/MovingButton_1.java
@@ -18,11 +18,12 @@ public class MovingButton_1 extends JFrame {
         c.setBackground(Color.DARK_GRAY);
         c.addKeyListener(new movingKeyListener());
 
-        jb.setLocation(MAX / 2, MAX / 2);
+        jb.setText("상,하,좌,우 키를 사용하여 움직여보세요!");
+        jb.setLocation(0, 0);
         jb.setSize(400, 50);
         c.add(jb);
 
-        setSize(MAX, MAX);
+        setSize(MAX * 2, MAX * 2);
         setVisible(true);
         c.setFocusable(true);
         c.requestFocus(); // 강제 포커스 부여.
@@ -38,26 +39,22 @@ public class MovingButton_1 extends JFrame {
                 case KeyEvent.VK_UP:
                     if (jb.getY() - MY_BUTTON >= 0) {
                         jb.setLocation(jb.getX(), jb.getY() - MY_BUTTON);
-                        jb.setText(MAX + " X " + MAX + "범위 내 이동 가능, 현재: " + jb.getX() + "," + jb.getY());
-                        break;
+                        jb.setText(MAX + " X " + MAX + "범위 내 이동 가능 / 단위: " + MY_BUTTON + " 현재 X: " + jb.getX() + " Y:" + jb.getY());
                     }break;
                 case KeyEvent.VK_DOWN:
                     if (jb.getY() + MY_BUTTON <= MAX) {
                         jb.setLocation(jb.getX(), jb.getY() + MY_BUTTON);
-                        jb.setText(MAX + " X " + MAX + "범위 내 이동 가능, 현재: " + jb.getX() + "," + jb.getY());
-                        break;
+                        jb.setText(MAX + " X " + MAX + "범위 내 이동 가능 / 단위: " + MY_BUTTON + " 현재 X: " + jb.getX() + " Y:" + jb.getY());
                     }break;
                 case KeyEvent.VK_LEFT:
                     if (jb.getX() - MY_BUTTON >= 0) {
                         jb.setLocation(jb.getX() - MY_BUTTON, jb.getY());
-                        jb.setText(MAX + " X " + MAX + "범위 내 이동 가능, 현재: " + jb.getX() + "," + jb.getY());
-                        break;
+                        jb.setText(MAX + " X " + MAX + "범위 내 이동 가능 / 단위: " + MY_BUTTON + " 현재 X: " + jb.getX() + " Y:" + jb.getY());
                     }break;
                 case KeyEvent.VK_RIGHT:
                     if (jb.getX() + MY_BUTTON <= MAX) {
                         jb.setLocation(jb.getX() + MY_BUTTON, jb.getY());
-                        jb.setText(MAX + " X " + MAX + "범위 내 이동 가능, 현재: " + jb.getX() + "," + jb.getY());
-                        break;
+                        jb.setText(MAX + " X " + MAX + "범위 내 이동 가능 / 단위: " + MY_BUTTON + " 현재 X: " + jb.getX() + " Y:" + jb.getY());
                     }break;
             }
         }
--- (Java)INU_2023_4_1/Java_Programming/src/HW_3/PrintWithButton.java
+++ (Java)INU_2023_4_1/Java_Programming/src/HW_3/PrintWithButton.java
@@ -6,46 +6,50 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class PrintWithButton extends JFrame {
-    private final int MAX = 700;
-    private final int BUTTON = 100;
-    private String[] num = {"0", "1", "2", "C"};
 
     JButton[] button = new JButton[4];
     JLabel la = new JLabel("", SwingConstants.CENTER);
     JPanel p = new JPanel();
 
     public PrintWithButton() throws HeadlessException {
+        String[] num = {"0", "1", "2", "C"};
+        int BUTTON = 100;
+        int MAX = 700;
         setTitle("press buttons to print / C for delete");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(MAX, MAX);
-        setVisible(true);
 
         Container c = getContentPane();
         c.setLayout(new BorderLayout());
 
         la.setHorizontalAlignment(SwingConstants.CENTER);
         la.setBackground(Color.LIGHT_GRAY);
-        la.setSize(30, 100);
+        la.setPreferredSize(new Dimension(30, 100));
         la.setOpaque(true);
         la.setVisible(true);
         c.add(la);
 
-        p.setBackground(Color.BLACK);
+        p.add(new JLabel("0 이후에 새로운 숫자의 입력은 허용되지 않습니다."), BorderLayout.NORTH);
+        p.setBackground(Color.ORANGE);
         p.setSize(MAX, 200);
         p.setVisible(true);
+
 
         MyMouseAdapter cl = new MyMouseAdapter();
         for (int i = 0; i < num.length; i++){
             button[i] = new JButton(num[i]);
+            button[i].setText(num[i]);
             button[i].setSize(BUTTON, BUTTON);
             button[i].addMouseListener(cl);
             p.add(button[i]);
         }
         c.add(p, BorderLayout.NORTH);
+
+        setSize(MAX, 200);
+        setVisible(true);
     }
     class MyMouseAdapter extends MouseAdapter {
         @Override
-        public void mouseClicked(MouseEvent e) {
+        public void mousePressed(MouseEvent e) {
             if(!la.getText().startsWith("0")) {
                 if (e.getSource() == button[0]) {
                     if (!la.getText().startsWith("0"))
